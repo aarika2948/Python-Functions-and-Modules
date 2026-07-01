16ae451a8e6702148c0414bc67a47ff8f00bc540
aarika2948 <jalanaarika@gmail.com>
2026-07-01 15:06:02 +0530

    Functions

---
+++ After Class Projects/circumference.py
@@ -0,0 +1,5 @@
+def c(x):
+    circumference= radius*(22/7)*2
+    return circumference
+radius= float(input("Enter the radius of your circle="))
+print(f"The circumference of a circle with the radius {radius} units is {c(radius)} units.")

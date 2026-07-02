81bd7eb13bbc2617252c7cc84658fdcf875cc579
aarika2948 <jalanaarika@gmail.com>
2026-07-02 15:31:10 +0530

    Keywords

---
+++ Keywords/break.py
@@ -0,0 +1,7 @@
+word=input("Enter the word=")
+for i in word:
+    if i=="a":
+        break
+    print(i)
+    print()
+    
---
+++ Keywords/continue.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the number="))
+x+=1
+while x>0:
+    x-=1
+    if x == 5:
+        continue
+    print(x)

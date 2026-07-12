bbbd1b21e83e40f456dff78ba4f34e6333853eb4
aarika2948 <jalanaarika@gmail.com>
2026-07-12 21:08:43 +0530

    A

--- After Class Projects/due.py
+++ After Class Projects/due.py
@@ -2,7 +2,7 @@ payed_amt=float(input("Enter the amount payed="))
 bill_sum=float(input("Enter the amount of the bill="))
 if (payed_amt>bill_sum):
     print("The shopkeeper has to return to the customer", payed_amt-bill_sum)
-elif(payed_sum==bil_sum):
+elif(payed_amt==bill_sum):
     pass
 else:
-    print()
+    print("The customer has to return to the shopkeeper", bill_sum-payed_amt)
